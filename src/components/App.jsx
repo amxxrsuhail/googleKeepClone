dbf2ed26ff47c33c872ab176de5a86cb14c90cfd
ameer suhail <kameer41099@gmail.com>
2022-12-15 13:44:09 +0530

    added notes.js and also added map function in app.jsx in order access the array and display it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(info) {
+  // console.log("successful")
+  return (
+    <Note key={info.key} title={info.title} content={info.content} />
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Footer />
-      <Note />
+      {notes.map(createNote)}
+      <Footer /> 
     </div>
   );
 }
