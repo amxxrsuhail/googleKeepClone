323c7f72ff64c2eff1e7e072844d41a63b88cc7e
ameer suhail <kameer41099@gmail.com>
2022-12-27 21:44:04 +0530

    added some changes in config

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb+srv://ameersuhail:${process.env.PASSWORD}@cluster0.wuwot5b.mongodb.net/?retryWrites=true&w=majority`,
+  mongoURI:
+    "mongodb+srv://ameersuhail:AMINAdavood41099@cluster0.wuwot5b.mongodb.net/?retryWrites=true&w=majority",
 };
