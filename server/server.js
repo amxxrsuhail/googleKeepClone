df5351f05ea99a81cf5a587e182893f3d8ac644f
ameer suhail <kameer41099@gmail.com>
2022-12-27 23:48:27 +0530

    added proxy and started using local db instead of atlas

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const config = require("./config/dev")
+// const config = require("./config/dev")
 
 // require("dotenv").config();
 
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // const uri = process.env.ATLAS_URI;
 // mongoose.connect(uri);
-mongoose.connect(config.mongoURI);
+mongoose.connect("mongodb://localhost:27017/keeperDB");
 
 const connection = mongoose.connection;
 connection.once("open", () => {
