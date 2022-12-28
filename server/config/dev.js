8f758c710f72fb9a1a19fd30580929ddb9be53d7
ameer suhail <kameer41099@gmail.com>
2022-12-28 16:28:19 +0530

    added back atlas

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://ameersuhail:AMINAdavood41099@cluster0.wuwot5b.mongodb.net/?retryWrites=true&w=majority",
-};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-// const config = require("./config/dev")
 
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 const port = 5000;
@@ -15,9 +14,9 @@ app.use(express.json());
 const path = require("path");
 app.use(express.static(path.join(__dirname, "client/build")))
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri);
-mongoose.connect("mongodb://localhost:27017/keeperDB");
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+// mongoose.connect("mongodb://localhost:27017/keeperDB");
 
 const connection = mongoose.connection;
 connection.once("open", () => {
