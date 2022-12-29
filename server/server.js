cd3887a10f20faf8b5c09e470b0618dff902234b
ameer suhail <kameer41099@gmail.com>
2022-12-29 12:42:12 +0530

    fully working in local system

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 // const config = require("./config/dev")
 
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 const port = 5000;
@@ -11,9 +11,9 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri);
-mongoose.connect("mongodb://localhost:27017/keeperDB");
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+// mongoose.connect("mongodb://localhost:27017/keeperDB");
 
 const connection = mongoose.connection;
 connection.once("open", () => {
